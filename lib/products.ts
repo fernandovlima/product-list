e38518725f482af652f9dd758e8cdb8273c25bc4
fernandovlima <fernandovlima.s@gmail.com>
2023-09-12 14:45:00 -0300

    add env

--- lib/products.ts
+++ lib/products.ts
@@ -1,4 +1,4 @@
 export const getProducts = async (page = 1) => {
-  const res = await fetch(`http://localhost:3000/api/products?page=${page}`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/products?page=${page}`)
   return res.json()
 }
