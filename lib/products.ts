19fd23a0f0875bc4096e09d4b77990d98f6a8e0a
fernandovlima <fernandovlima.s@gmail.com>
2023-09-12 16:04:01 -0300

    revalidate getProducts

--- lib/products.ts
+++ lib/products.ts
@@ -1,6 +1,6 @@
 export const getProducts = async (page = 1) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/api/products?page=${page}`, {
-    next: { revalidate: 3600 }
+    next: { revalidate: 60 }
   })
   return await res.json()
 }
